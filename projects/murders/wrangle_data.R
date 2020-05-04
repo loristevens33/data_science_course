66110bdab19b493c0ee480f0480903027c41c253
loristevens33 <lori_stevens33@yahoo.com>
2020-05-04 12:25:19 -0400

    Added murders directory

---
+++ projects/murders/wrangle_data.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+murders <- read_csv("projects/murders/data/murders.csv")
+murders <- murders %>% mutate(region = factor(region), rate = total / population * 10^5)
+save(murders, file = "projects/murders/rda/murders.rda")
